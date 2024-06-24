473b84aa3d9d440fc685b7f77234d781ac2496f9
LamesginewAndargie <lame2002@gmail.com>
2024-06-24 09:11:29 +0300

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import Baner from "../../Components/Banner/Baner";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Baner />
 			<Footer />
 		</>
 	);
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,14 +1,14 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_ApiKey; // this method of using API_KEYis not working. Why?
+const API_KEY="c39ca3ce77cec930f8cee774076db769";
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
 	fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
 	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
 	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
 	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
 	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-	fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
